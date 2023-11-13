ecefedecae330b9a2feebbf0392f0d7e810a459b
Frankllin151 <frankllinsilva300@gamil.com>
2023-11-13 15:23:39 -0300

    first commit

---
+++ assets/js/painel.js
@@ -0,0 +1,32 @@
+function w3_open() {
+    document.getElementById("main-header").style.marginLeft = "25%";
+    document.getElementById("main-site").style.marginLeft = "25%";
+    document.getElementById("mySidebar").style.width = "25%";
+    document.getElementById("mySidebar").style.display = "block";
+    
+    document.getElementById("openNav").style.display = 'none';
+  }
+  function w3_close() {
+    document.getElementById("main-site").style.marginLeft ="0%"
+    document.getElementById("main-header").style.marginLeft = "0%";
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("openNav").style.display = "inline-block";
+  }
+
+
+
+
+  function includeHTML() {
+    const headerElement = document.getElementById("header");
+    const pageAgenda = document.getElementById("page-agenda");
+
+    fetch("header.html")
+       .then(response => response.text())
+       .then(data => headerElement.innerHTML = data);
+
+       fetch("agenda-prio.html")
+       .then(response => response.text())
+       .then(data => pageAgenda.innerHTML = data);
+    
+ }
+ window.onload = includeHTML;
